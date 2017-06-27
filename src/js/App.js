9f97048ed88688cb07e58adfe792994eb502a091
pipopotamasu <ec0156hx@gmail.com>
2017-06-28 07:56:43 +0900

    add Counter App

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Counter from './Counter'
 import logo from '../image/logo.svg';
 import '../css/App.css';
 
@@ -10,9 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Apps">
+          <Counter />
+        </div>
       </div>
     );
   }
